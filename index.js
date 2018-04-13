dcc0bd60bc18920cf62b9015006179df07ea49df
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-13 23:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function currentLine(katzDeli) {
   for (var i = 0; i < katzDeli.length; i++) {
     var elements = [];
      elements.push(parseInt(i+1) + ". " + katzDeli[i]);
+     var y = elements.join(", ")
   }
   console.log("The line is current:" + elements.join(", "))
 }
